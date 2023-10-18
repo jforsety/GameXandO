3f88056bb856461cd9afc3bc03a4f38d184fd0c0
Name <email@example.ru>
2023-10-18 22:43:40 +0300

    game

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ text = """_____________________
 ---------------------
 """
 
-# Задаем вложенный список нашего поля
-field = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Задаем список поля
+field = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 # Функция победных комбинаций
@@ -19,21 +19,21 @@ def check_win(field):
            [0, 4, 8],
            [2, 4, 6]]
     for sign in win:
-        if field[0][sign[0]] == field[0][sign[1]] == field[0][sign[2]]:
-            return field[0][sign[0]]
-        elif field[1][sign[0]] == field[1][sign[1]] == field[1][sign[2]]:
-            return field[1][sign[0]]
-        elif field[2][sign[0]] == field[2][sign[1]] == field[2][sign[2]]:
-            return field[2][sign[0]]
+        if field[sign[0]] == field[sign[1]] == field[sign[2]]:
+            return field[sign[0]]
+        elif field[sign[0]] == field[sign[1]] == field[sign[2]]:
+            return field[sign[0]]
+        elif field[sign[0]] == field[sign[1]] == field[sign[2]]:
+            return field[sign[0]]
     return False
 
 
 # Функция игрового поля
 def game_field(field):
     print(f"--------------\n"
-          f"| {field[0][0]} | {field[0][1]} | {field[0][2]} |\n"
-          f"| {field[1][0]} | {field[1][1]} | {field[1][2]} |\n"
-          f"| {field[2][0]} | {field[2][1]} | {field[2][2]} |\n"
+          f"| {field[0]} | {field[1]} | {field[2]} |\n"
+          f"| {field[3]} | {field[4]} | {field[5]} |\n"
+          f"| {field[6]} | {field[7]} | {field[8]} |\n"
           f"--------------\n")
 
 
@@ -42,24 +42,12 @@ def check(token):
     value = False
     count = range(1, 10)
     while not value:
-        player_input = int(input("Куда будем ставить " + token + "?: "))
+        player_input = int(input("Куда будем ставить " + token + " ?: "))
         if player_input not in count:
             print("Вы ввели неверные координаты")
-        elif 1 <= player_input <= 3:
-            if str(field[0][player_input - 1]) not in "XO":
-                field[0][player_input - 1] = token
-                value = True
-            else:
-                print("Эта клетка уже занята!\n")
-        elif 4 <= player_input <= 6:
-            if str(field[1][player_input - 4]) not in "XO":
-                field[1][player_input - 4] = token
-                value = True
-            else:
-                print("Эта клетка уже занята!\n")
-        elif 7 <= player_input <= 9:
-            if str(field[2][player_input - 7]) not in "XO":
-                field[2][player_input - 7] = token
+        elif 1 <= player_input <= 9:
+            if str(field[player_input - 1]) not in "XO":
+                field[player_input - 1] = token
                 value = True
             else:
                 print("Эта клетка уже занята!\n")
@@ -68,31 +56,34 @@ def check(token):
 
 
 # Функция основного цикла игры
-def main(field):
-    counter = 0
-    win = False
-    while not win:
-        game_field(field)
-        if counter % 2 == 0:
-            check("X")
-        else:
-            check("O")
-        counter += 1
-        if counter > 4:
-            result = check_win(field)
-            if result:
-                print(f"Победитель {result} !!!\n")
+def next_game(field):
+    def main(field):
+        counter = 0
+        win = False
+        while not win:
+            game_field(field)
+            if counter % 2 == 0:
+                check("X")
+            else:
+                check("O")
+            counter += 1
+            if counter > 4:
+                result = check_win(field)
+                if result:
+                    print(f"Победитель {result} !!!\n")
+                    game_field(field)
+                    break
+            elif counter == 9:
+                print("Ничья!\n")
+                game_field(field)
                 break
-        elif counter == 9:
-            print("Ничья!\n")
-            break
-
-
-
-# Выводим начальный текст, главную функцию, ввод для завершения игры, либо начала новой игры.
-print(text)
-main(field)
-key = input("Нажмите любую клавишу для выхода!\nЕсли хотите начать заново, то введите слово 'начать': ")
-if key == "начать":
-    print(text)
     main(field)
+    key = input("Нажмите любую клавишу для выхода!\nЕсли хотите начать заново, то введите слово 'начать': ")
+    if key == "начать":
+        print(text)
+        field = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        main(field)
+
+# Выводим начальный текст, главную функцию.
+print(text)
+next_game(field)
